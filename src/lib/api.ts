2bff73f259dfdce9836f769583928fc1d74b780e
Christopher Geary <hello@crgeary.com>
2022-05-07 10:50:17 +0100

    Add utils for accessing api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { ExtendedTrackDto, TrackDto } from "../types/api";
 
 export class HttpError extends Error {
     constructor(public code: number, message?: string) {
@@ -23,4 +25,12 @@ export const wrapper = (cb: NextApiHandler) => {
             });
         }
     };
+};
+
+export const getTracks = async () => {
+    return (await axios.get<{ data: TrackDto[] }>("/api/tracks")).data;
+};
+
+export const getTrack = async (id: string) => {
+    return (await axios.get<{ data: ExtendedTrackDto }>(`/api/tracks/${id}`)).data;
 };
